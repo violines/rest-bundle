64fe0deb9a6ba1ae626a951abe38b8a58d3ef73c
Simon Schubert <si.schubert@live.de>
2019-12-26 20:41:59 +0100

    Add BundleClass + DependencyInjection base

---
+++ DependencyInjection/TerryApiExtension.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace TerryApiBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+
+class TerryApiExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        //
+    }
+}
---
+++ TerryApiBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace TerryApiBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class TerryApiBundle extends Bundle
+{
+}
