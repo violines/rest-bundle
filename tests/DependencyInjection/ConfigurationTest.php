a693bb229d90a6f3f99f7c29da10c06c00af5f66
Simon Schubert <48808627+simon-schubert@users.noreply.github.com>
2020-04-18 15:49:47 +0200

    test symfony configuration (#41)

---
+++ tests/DependencyInjection/ConfigurationTest.php
@@ -0,0 +1,158 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TerryApi\Tests\Builder;
+
+use PHPUnit\Framework\TestCase;
+use TerryApiBundle\DependencyInjection\Configuration;
+
+class ConfigurationTest extends TestCase
+{
+    /**
+     * @dataProvider providerShouldCheckConfiguration
+     */
+    public function testShouldCheckConfiguration(array $input, array $expected)
+    {
+        $configuration = new Configuration();
+
+        $node = $configuration
+            ->getConfigTreeBuilder()
+            ->buildTree();
+
+        $finalized = $node->finalize($node->normalize($input));
+
+        $this->assertEquals($expected, $finalized);
+    }
+
+    public function providerShouldCheckConfiguration()
+    {
+        return [
+            [
+                [],
+                [
+                    'event_listener' => [
+                        'http_error_listener' => [
+                            'enable' => true,
+                        ],
+                        'response_transform_listener' => [
+                            'enable' => true,
+                        ],
+                    ],
+                    'argument_value_resolver' => [
+                        'abstract_http_client_resolver' => [
+                            'enable' => true,
+                        ],
+                        'request_single_struct_resolver' => [
+                            'enable' => true,
+                        ],
+                        'request_array_of_structs_resolver' => [
+                            'enable' => true,
+                        ],
+                    ],
+                    'http_server' => [
+                        'format_default' => null,
+                    ],
+                ],
+            ],
+            [
+                [
+                    'http_server' => [
+                        'formats' => [
+                            'json' => [
+                                'application/json',
+                                'application/json+ld'
+                            ],
+                            'xml' => [
+                                'application/xml',
+                                'text/html'
+                            ]
+                        ],
+                        'format_default' => 'application/json',
+                    ]
+                ],
+                [
+                    'event_listener' => [
+                        'http_error_listener' => [
+                            'enable' => true,
+                        ],
+                        'response_transform_listener' => [
+                            'enable' => true,
+                        ],
+                    ],
+                    'argument_value_resolver' => [
+                        'abstract_http_client_resolver' => [
+                            'enable' => true,
+                        ],
+                        'request_single_struct_resolver' => [
+                            'enable' => true,
+                        ],
+                        'request_array_of_structs_resolver' => [
+                            'enable' => true,
+                        ],
+                    ],
+                    'http_server' => [
+                        'formats' => [
+                            'json' => [
+                                'application/json',
+                                'application/json+ld'
+                            ],
+                            'xml' => [
+                                'application/xml',
+                                'text/html'
+                            ]
+                        ],
+                        'format_default' => 'application/json',
+                    ],
+                ],
+            ],
+            [
+                [
+                    'event_listener' => [
+                        'http_error_listener' => [
+                            'enable' => false,
+                        ],
+                        'response_transform_listener' => [
+                            'enable' => false,
+                        ],
+                    ],
+                    'argument_value_resolver' => [
+                        'abstract_http_client_resolver' => [
+                            'enable' => false,
+                        ],
+                        'request_single_struct_resolver' => [
+                            'enable' => false,
+                        ],
+                        'request_array_of_structs_resolver' => [
+                            'enable' => false,
+                        ],
+                    ]
+                ],
+                [
+                    'event_listener' => [
+                        'http_error_listener' => [
+                            'enable' => false,
+                        ],
+                        'response_transform_listener' => [
+                            'enable' => false,
+                        ],
+                    ],
+                    'argument_value_resolver' => [
+                        'abstract_http_client_resolver' => [
+                            'enable' => false,
+                        ],
+                        'request_single_struct_resolver' => [
+                            'enable' => false,
+                        ],
+                        'request_array_of_structs_resolver' => [
+                            'enable' => false,
+                        ],
+                    ],
+                    'http_server' => [
+                        'format_default' => null,
+                    ],
+                ],
+            ],
+        ];
+    }
+}
