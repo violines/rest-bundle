4d523c1506f403d3062440c6b2b797daf30dbff8
Simon Schubert <si.schubert@live.de>
2020-04-19 14:11:49 +0200

    test TerryApiExtension

---
+++ tests/DependencyInjection/TerryApiExtensionTest.php
@@ -0,0 +1,80 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TerryApi\Tests\DependencyInjection;
+
+use Matthias\SymfonyDependencyInjectionTest\PhpUnit\AbstractExtensionTestCase;
+use TerryApiBundle\DependencyInjection\TerryApiExtension;
+
+class TerryApiExtensionTest extends AbstractExtensionTestCase
+{
+    /**
+     * @dataProvider providerForEntryPointServiceIds
+     */
+    public function testShouldCheckDefaultServiceLoad(string $serviceId)
+    {
+        $this->load();
+
+        $this->assertContainerBuilderHasService($serviceId);
+    }
+
+    /**
+     * @dataProvider providerForEntryPointServiceIds
+     */
+    public function testShouldCheckServicesNotLoaded(string $serviceId)
+    {
+        $parts = explode('.', $serviceId);
+        $definition[$parts[1]][$parts[2]]['enable'] = false;
+
+        $this->load($definition);
+
+        $this->assertContainerBuilderNotHasService($serviceId);
+    }
+
+    public function providerForEntryPointServiceIds()
+    {
+        return [
+            [
+                'terry_api.event_listener.http_error_listener'
+            ],
+            [
+                'terry_api.event_listener.response_transform_listener'
+            ],
+            [
+                'terry_api.argument_value_resolver.abstract_http_client_resolver'
+            ],
+            [
+                'terry_api.argument_value_resolver.request_single_struct_resolver'
+            ],
+            [
+                'terry_api.argument_value_resolver.request_array_of_structs_resolver'
+            ]
+        ];
+    }
+
+    /**
+     * @dataProvider providerForEntryPointServiceIds
+     */
+    public function testCreateHTTPServerDefinition()
+    {
+        $this->load();
+
+        $this->assertContainerBuilderHasServiceDefinitionWithArgument(
+            'terry_api.factory.http_server_factory',
+            0,
+            [
+                'formats' => [
+                    'json' => [],
+                    'xml' => []
+                ],
+                'format_default' => '',
+            ]
+        );
+    }
+
+    protected function getContainerExtensions(): array
+    {
+        return [new TerryApiExtension()];
+    }
+}
