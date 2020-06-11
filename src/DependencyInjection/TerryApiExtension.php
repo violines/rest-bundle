b423e4d26a8b45dac21b68df9047d8a69eaa28e4
Simon Schubert <si.schubert@live.de>
2020-06-11 09:18:17 +0200

    move service definition into 1 xml

--- src/DependencyInjection/TerryApiExtension.php
+++ src/DependencyInjection/TerryApiExtension.php
@@ -14,11 +14,7 @@ class TerryApiExtension extends Extension
     public function load(array $configs, ContainerBuilder $container): void
     {
         $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
-        $loader->load('factory.xml');
-        $loader->load('annotation.xml');
-        $loader->load('builder.xml');
-        $loader->load('listener.xml');
-        $loader->load('resolver.xml');
+        //$loader->load('factory.xml');
         $loader->load('service.xml');
 
         /** @var array<string, array<string, array<string,mixed>>> $processedConfigs */
